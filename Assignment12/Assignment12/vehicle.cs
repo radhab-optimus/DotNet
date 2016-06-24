2cd3853f97cd217dbc3dda08aac476ae04eb8991
radhab-optimus <radhabisht@optimusinfo.com>
2016-06-24 11:17:02 +0530

    code for assignment 12

---
+++ Assignment12/Assignment12/vehicle.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+
+namespace Assignment12
+{
+    public class Vehicle : IComparable
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public float Speed { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Vehicle vehicleObj = obj as Vehicle;  
+            return this.Name.CompareTo(vehicleObj.Name);
+        }
+        
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Vehicle))
+            {
+                return false;
+            }
+            Vehicle vehicle = obj as Vehicle;
+            return Name == vehicle.Name && Speed == vehicle.Speed;
+        }
+
+        public static void Main()
+        {
+            // Creating objects of vehicle class
+            var truck = new Vehicle();
+            truck.Name = "Leyland";
+            truck.Model = "SS2";
+            truck.Speed = 34;
+
+            Vehicle car = new Vehicle();
+            car.Name = "Audi";
+            car.Model = "c2";
+            car.Speed = 111;
+
+            Vehicle bike = new Vehicle();
+            bike.Name = "TVS";
+            bike.Model = "ss3";
+            bike.Speed = 55;
+
+            Vehicle bicycle = new Vehicle();
+            bicycle.Name = "TVS";
+            bicycle.Model = "sss3";
+            bicycle.Speed = 55;
+
+            // Adding objects into ArrayList
+            ArrayList vehicleList = new ArrayList();
+            vehicleList.Add(truck);
+            vehicleList.Add(car);
+            vehicleList.Add(bike);
+
+            // Comparing two objects
+            int result = truck.CompareTo(car);
+            if (result == 0)
+            {
+                Console.WriteLine("Objects are equal");
+            }
+            else
+            {
+                Console.WriteLine("Objects are not equal");
+            }
+
+            // Before sorting objects
+            foreach (Vehicle v1 in vehicleList)
+            {
+                Console.WriteLine("Name: {0},  Model: {1}, Speed: {2}", v1.Name, v1.Model, v1.Speed);
+            }
+
+            // After sorting objects
+            vehicleList.Sort();
+            Console.WriteLine("after sorting");
+            foreach( Vehicle v in vehicleList )
+            {
+                Console.WriteLine("Name: {0},  Model: {1}, Speed: {2}", v.Name, v.Model, v.Speed);
+            }
+
+            // Using overloaded equal function
+            Console.WriteLine("Checking car and bike objects of vehical are equal or not");
+            Console.WriteLine("Result {0}", car.Equals(bike));
+            Console.WriteLine("Checking bike and bicycle objects of vehical are equal or not");
+            Console.WriteLine("Result {0}", bike.Equals(bicycle));
+        }
+    }
+}
---
+++ Assignment12/Assignment12/vehicles.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Assignment12
+{
+    class VehicleCollections : IEnumerable
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int Speed { get; set; }
+        private List<VehicleCollections> vehicleList;
+
+        public static void Main()
+        {
+            VehicleCollections vehicle1 = new VehicleCollections()
+            {
+                Name = "vehicle1",
+                Model = "model1",
+                Speed = 21
+            };
+
+            VehicleCollections vehicle2 = new VehicleCollections()
+            {
+                Name = "vehicle2",
+                Model = "model2",
+                Speed = 212
+            };
+
+            VehicleCollections vehicle3 = new VehicleCollections()
+            {
+                Name = "vehicle3",
+                Model = "model3",
+                Speed = 211
+            };
+
+            List<VehicleCollections> vehicleList = new List<VehicleCollections>(3)
+            {
+                vehicle1,
+                vehicle2,
+                vehicle3
+            };
+
+            foreach (VehicleCollections v in vehicleList)
+            {
+                Console.WriteLine("Name: {0}, Model: {1}, Speed: {2}", v.Name, v.Model, v.Speed);
+            }
+
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return vehicleList.GetEnumerator();
+        }
+    }
+}
